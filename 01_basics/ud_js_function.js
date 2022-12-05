3d51fbd81f673dbb57f57526fb39ba861dde256c
SurpassYourLimits <surpassyourlimits0@gmail.com>
2022-12-04 20:54:10 -0600

    fd:

--- 01_basics/ud_js_function.js
+++ 01_basics/ud_js_function.js
@@ -13,17 +13,72 @@
 // const res = convert(500, usdCurr);
 // promotion(res);
 
-const usdCurr = 75;
-const discount = 0.9;
-function convert(amount) {
-    let carr = 
-    return function (curr) {
-        return amount * curr;
-    };
+// const usdCurr = 75;
+// const discount = 0.9;
+// function convert(amount, curr) {
+
+//     return amount * curr;
+// }
+
+// function promotion(result) {
+//     console.log(result * discount);
+// }
+// const res = convert(500, usdCurr);
+// promotion(res);
+
+// function test () {
+//     for (let i = 0; i < 5; i++) {
+//         console.log(i);
+//         if (i === 3) {
+//             return;
+//         }
+
+//     } console.log("doneee");
+// }
+
+// test();
+
+// // 
+// function doNothing() {};
+// console.log(doNothing()=== undefined);
+
+// work Exercise 6:
+//1
+function sayHello(name) {
+    return `Привет ${name}!`;
+}
+sayHello('Erlan');
+
+//2
+function inBetween(a) {
+    return [a - 1, a, a + 1];
+
+}
+inBetween();
+console.log(inBetween(5));
+
+//3
+function getMathResult(num, progressive) {
+    // let sum = 0;
+    // return num + num, progressive++;
+    for (let i = 0; i <= num; i++) {
+        for (let j = 0; i+i; j <= progressive; i+i) {
+            console.log(num, progressive);
+        }
+    }
+}
+getMathResult();
+console.log(getMathResult(5, 3));
+
+numbers = [1, 2, 3, 4, 5];
+
+function doubling(number) {
+    number *= 2;
+    return number;
 }
 
-function promotion(result) {
-    console.log(result * discount);
-}
-const res = convert(500, usdCurr);
-promotion(res);
+obj = {};
+for (var i = 0; i < numbers.length; i++)
+    doubled = doubling(numbers[i]);
+obj[numbers[i]] = doubled;
+console.log(obj);
