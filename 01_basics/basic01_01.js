d0e1c2ba7258374dc393cbc53ae5ccd777836c2b
SurpassYourLimits <surpassyourlimits0@gmail.com>
2022-12-09 12:18:40 -0600

    Hours

--- 01_basics/basic01_01.js
+++ 01_basics/basic01_01.js
@@ -1,95 +1,132 @@
 //numsbers
-let billion = 1e11;
+// let billion = 1e11;
 
-console.log(billion);
+// console.log(billion);
 
-let ms = 0.000001; //long ver
+// let ms = 0.000001; //long ver
 
-let mse = 1e-6; // шесть нулей, слева от 1
+// let mse = 1e-6; // шесть нулей, слева от 1
 
-// Work 
+// // Work 
 
-let numberOfFilms;
+// let numberOfFilms;
 
-function start() {
-   numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+// function start() {
+//    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-   while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-      numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-   }
+//    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+//       numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+//    }
+// }
+
+// start();
+
+// const personalMovieDB = {
+//    count: numberOfFilms,
+//    movies: { 'John Wick': '8.8' },
+//    actors: {},
+//    genres: [],
+//    private: false
+// };
+
+// function rememberMyFilms() {
+//    for (let i = 0; i < 2; i++) {
+//       const a = prompt('Один из опследних просмотренных фильмов?', ''),
+//          b = prompt('На сколько оцените его?', '');
+//       if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//          personalMovieDB.movies[a] = b;
+//          console.log('done!');
+//       } else {
+//          i--;
+//          console.log('error!');
+//       }
+
+//    }
+// }
+
+// rememberMyFilms();
+
+// function detectPersonalLevel() {
+//    if (numberOfFilms < 10) {
+//       console.log("Просмотрено довольно мало фильмов");
+//    } else if (numberOfFilms >= 10 && numberOfFilms < 30) {
+//       console.log("Вы классический зритель");
+//    } else if (numberOfFilms >= 30) {
+//       console.log("Вы киноман!");
+//    } else {
+//       console.log("Error");
+//    }
+// }
+
+// detectPersonalLevel();
+
+// function showMyDB(hidden) {
+//    if (!hidden) {
+//       console.log(personalMovieDB);
+//    }
+// }
+// showMyDB(personalMovieDB.private);
+
+// function writeYourGeneres() {
+//    for (let i = 1; i <= 3; i++) {
+
+//       personalMovieDB.genres[i - 1] = prompt(`your faavorite in num:${i}`);
+
+//    }
+// }
+// writeYourGeneres();
+
+// // Problem!
+
+// function calculateVolumeAndArea(cub) {
+//    if (typeof (cub) !== 'number' || cub < 0 || !Number.isInteger(cub)) {
+//       return "error";
+//    }
+//    let volume = 0,
+//       area = 0;
+
+//    volume = cub * cub * cub;
+//    area = 6 * (cub * cub);
+
+//    return `Объем куба: ${volume}, площадь всей поверхности: ${area}`;
+
+// }
+// calculateVolumeAndArea(5);
+// console.log(calculateVolumeAndArea(5));
+
+
+// function trine(seatNum) {
+//     if (typeof (seatNum) !== 'number' || seatNum < 0 || !Number.isInteger(seatNum)) {
+//         return "При вычислении произошла ошибка";
+//     } else if (seatNum === 0 || seatNum > 36) {
+//         return "Таких мест в вагоне не существует";
+//     }
+
+//     return Math.ceil(seatNum / 4);
+
+// }
+// trine();
+// console.log(trine(30));
+
+//JavaScript clock!!!!!!!!!!
+
+function getTimeFromMinutes(timesTamp) {
+    if (typeof (timesTamp) !== 'number' || timesTamp < 0 || !Number.isInteger(timesTamp)) {
+        return "Ошибка, проверьте данные";
+    }
+
+    let hour = Math.floor(timesTamp / 60);
+    let min = timesTamp % 60;
+
+    return `"Это ${hour} часа и  ${min} минут"`;
+
+}// Место для первой задачи
+
+getTimeFromMinutes();
+console.log(getTimeFromMinutes(665));
+
+function getMaxOfArray(numArray) {
+    return Math.max.apply(null, numArray);
 }
-
-start();
-
-const personalMovieDB = {
-   count: numberOfFilms,
-   movies: { 'John Wick': '8.8' },
-   actors: {},
-   genres: [],
-   private: false
-};
-
-function rememberMyFilms() {
-   for (let i = 0; i < 2; i++) {
-      const a = prompt('Один из опследних просмотренных фильмов?', ''),
-         b = prompt('На сколько оцените его?', '');
-      if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-         personalMovieDB.movies[a] = b;
-         console.log('done!');
-      } else {
-         i--;
-         console.log('error!');
-      }
-
-   }
-}
-
-rememberMyFilms();
-
-function detectPersonalLevel() {
-   if (numberOfFilms < 10) {
-      console.log("Просмотрено довольно мало фильмов");
-   } else if (numberOfFilms >= 10 && numberOfFilms < 30) {
-      console.log("Вы классический зритель");
-   } else if (numberOfFilms >= 30) {
-      console.log("Вы киноман!");
-   } else {
-      console.log("Error");
-   }
-}
-
-detectPersonalLevel();
-
-function showMyDB(hidden) {
-   if (!hidden) {
-      console.log(personalMovieDB);
-   }
-}
-showMyDB(personalMovieDB.private);
-
-function writeYourGeneres() {
-   for (let i = 1; i <= 3; i++) {
-
-      personalMovieDB.genres[i - 1] = prompt(`your faavorite in num:${i}`);
-
-   }
-}
-writeYourGeneres();
-
-// Problem!
-
-function calculateVolumeAndArea(cub) {
-   if (typeof (cub) !== 'number' || cub < 0 || !Number.isInteger(cub)) {
-      return "error";
-   }
-   let volume = 0,
-      area = 0;
-
-   volume = cub * cub * cub;
-   area = 6 * (cub * cub);
-
-   return `Объем куба: ${volume}, площадь всей поверхности: ${area}`;
-
-}
-calculateVolumeAndArea(5);
-console.log(calculateVolumeAndArea(5));
+getMaxOfArray();
+console.log(getMaxOfArray(12, 16, 87));
