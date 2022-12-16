a31be252548d8795cafae39e57b23d090134a7d5
surpassyourlimits <surpassyourlimits0@gamil.com>
2022-12-16 11:03:08 +0600

    methods

--- 01_basics/basic01_03.js
+++ 01_basics/basic01_03.js
@@ -1,8 +1,16 @@
 let rabbit = {};
 rabbit.run = function (n) {
-    console.log("Run " + n + " meters");
+   console.log("Run " + n + " meters");
 };
 rabbit.run(60);
 /*******Методы - это функции, хранящиеся в виде свойств объекта.*/
 
-function this1() { dddddddddds;
+function d(d) {
+
+}
+alert('Hello World');
+
+function person(some) {
+   alert('hello');
+} 
+console.log('erlan');
